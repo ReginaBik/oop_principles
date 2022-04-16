35c38f551b815a21a7da42441b20690b44e37134
ReginaBik <reginabiktimirova1@gmail.com>
2022-04-16 11:39:54 -0500

    added employee practice

---
+++ src/encapsulation/Employee.java
@@ -0,0 +1,66 @@
+package encapsulation;
+
+public class Employee {
+    // default constructor
+    public Employee(){
+
+    }
+
+    //instance variables
+    private final static int totalEmployees = 10;
+
+    public static int getTotalEmployees(){
+        return totalEmployees;
+    }
+
+    private String fullName;
+
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private String address;
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    private String phone;
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
